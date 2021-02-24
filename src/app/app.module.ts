4e077b58211150bdc817fafe3ca280a3954688c2
elizabeth <elizabethpt2020@ec.ajce.in>
2021-02-24 14:28:18 +0530

    edit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EnquiryComponent } from './enquiry/enquiry.component';
+import { HistoryComponent } from './history/history.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EnquiryComponent,
+    HistoryComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
